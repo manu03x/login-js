a0ea2a67ec3a858e124482be64b992191b91d10b
Emmanuel Ruiz <eruiz3@ucol.mx>
2023-05-16 09:00:27 -0500

    login auth

---
+++ config.js
@@ -0,0 +1,7 @@
+const dotenv = require('dotenv')
+dotenv.config();
+
+export const NODE_ENV = process.env.NODE_ENV || 'development';
+export const HOST = process.env.HOST || '127.0.0.1';
+export const PORT = process.env.PORT || 3000;
+
---
+++ index.js
@@ -0,0 +1,19 @@
+// index.js
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+// set up port
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cors());
+
+// add routes
+const router = require('./routes/router.js');
+app.use('/api', router);
+
+// run server
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
